345fa98569bb6bdcebf0cabc468a1eb4ddbee9c4
odakris <christopher_denis@live.fr>
2025-01-30 16:21:51 +0100

    implementation API CRUD pour la table Produits

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mysql = require("mysql2/promise");
+// const fs = require("fs");
 
 const app = express();
 app.use(express.json());
@@ -47,7 +48,44 @@ app.delete("/categories/:id", async (req, res) => {
   const connection = await dbConnection();
   const { id } = req.params;
   await connection.query(`DELETE FROM Categories WHERE id = '${id}'`);
-  res.send("Categorie mise à jour");
+  res.send("Categorie supprimée");
+});
+
+// PRODUITS CRUD //////////////////////////////////////////////////////////////////////////////
+// GET
+app.get("/produits", async (req, res) => {
+  const connection = await dbConnection();
+  const [produits] = await connection.query("SELECT * FROM produits");
+  res.json(produits);
+});
+
+// POST
+app.post("/produits", async (req, res) => {
+  const connection = await dbConnection();
+  const { reference, nom, description_produit, prix_unitaire, quantite_stock, id_categorie, id_fournisseur } = req.body;
+  await connection.query(
+    `INSERT INTO Produits (reference, nom, description_produit, prix_unitaire, quantite_stock, id_categorie, id_fournisseur) VALUES ('${reference}','${nom}','${description_produit}','${prix_unitaire}','${quantite_stock}','${id_categorie}','${id_fournisseur}')`
+  );
+  res.send("Produit ajouté");
+});
+
+// PUT
+app.put("/produits/:id", async (req, res) => {
+  const connection = await dbConnection();
+  const { id } = req.params;
+  const { reference, nom, description_produit, prix_unitaire, quantite_stock, id_categorie, id_fournisseur } = req.body;
+  await connection.query(
+    `UPDATE Produits SET reference = '${reference}', nom = '${nom}', description_produit = '${description_produit}', prix_unitaire = '${prix_unitaire}', quantite_stock = '${quantite_stock}', id_categorie = '${id_categorie}', id_fournisseur = '${id_fournisseur}' WHERE id = '${id}'`
+  );
+  res.send("Produit mis à jour");
+});
+
+// DELETE
+app.delete("/produits/:id", async (req, res) => {
+  const connection = await dbConnection();
+  const { id } = req.params;
+  await connection.query(`DELETE FROM Produits WHERE id = '${id}'`);
+  res.send("Produit supprimé");
 });
 
 // Démarrage du serveur
