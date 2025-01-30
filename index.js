d52152e45f4824e6e1053c81994b2d20620f8fd4
odakris <christopher_denis@live.fr>
2025-01-30 15:50:34 +0100

    implementation API CRUD pour la table Categories

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const mysql = require("mysql2/promise");
-const fs = require("fs");
 
 const app = express();
 app.use(express.json());
@@ -9,37 +8,50 @@ const dbConfig = {
   host: "localhost",
   user: "root",
   password: "root",
-  multipleStatements: true,
+  database: "avion_papier",
 };
 
-const executeSQLFile = async (connection, filePath) => {
-  const sql = fs.readFileSync(filePath, "utf8");
-  await connection.query(sql);
-  console.log(`${filePath} exécuté avec succès`);
+// Connection à la base de données
+const dbConnection = async () => {
+  const connection = await mysql.createConnection(dbConfig);
+  return connection;
 };
 
-const initDB = async () => {
-  try {
-    const connection = await mysql.createConnection(dbConfig);
-    console.log("Connexion à MySQL réussie");
+// CATEGORIES CRUD //////////////////////////////////////////////////////////////////////////////
+// GET
+app.get("/categories", async (req, res) => {
+  const connection = await dbConnection();
+  const [categories] = await connection.query("SELECT * FROM Categories");
+  res.json(categories);
+});
 
-    await executeSQLFile(connection, "db.sql");
+// POST
+app.post("/categories", async (req, res) => {
+  const connection = await dbConnection();
+  const { nom } = req.body;
+  await connection.query(`INSERT INTO Categories (nom) VALUES ('${nom}')`);
+  res.send("Categorie ajoutée");
+});
 
-    await connection.changeUser({ database: "avion_papier" });
+// PUT
+app.put("/categories/:id", async (req, res) => {
+  const connection = await dbConnection();
+  const { id } = req.params;
+  const { nom } = req.body;
+  await connection.query(`UPDATE Categories SET nom = '${nom}' WHERE id = '${id}'`);
+  res.send("Categorie mise à jour");
+});
 
-    await executeSQLFile(connection, "data.sql");
+// DELETE
+app.delete("/categories/:id", async (req, res) => {
+  const connection = await dbConnection();
+  const { id } = req.params;
+  await connection.query(`DELETE FROM Categories WHERE id = '${id}'`);
+  res.send("Categorie mise à jour");
+});
 
-    console.log("Base de données initialisée avec succès");
-    return connection;
-  } catch (err) {
-    console.error("Erreur lors de l'initialisation de la base de données :", err);
-    process.exit(1);
-  }
-};
-
-initDB().then((connection) => {
-  const PORT = 3000;
-  app.listen(PORT, () => {
-    console.log(`Serveur démarré sur le port ${PORT}`);
-  });
+// Démarrage du serveur
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Serveur démarré sur le port ${PORT}`);
 });
