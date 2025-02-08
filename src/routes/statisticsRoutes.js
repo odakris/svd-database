1d7834536c42ac8718af7b990b75fa8fdf2be12e
odakris <christopher_denis@live.fr>
2025-02-08 18:49:33 +0100

    netoyage des routes statistiques

--- src/routes/statisticsRoutes.js
+++ src/routes/statisticsRoutes.js
@@ -7,9 +7,4 @@ router.get("/top-clients", statisticsController.getTopClients);
 router.get("/top-fournisseurs", statisticsController.getTopSuppliers);
 router.get("/total-ventes", statisticsController.getTotalSales);
 
-// router.get("/:id", categoriesController.getCategoryById);
-// router.post("/", categoriesController.createCategory);
-// router.put("/:id", categoriesController.updateCategory);
-// router.delete("/:id", categoriesController.deleteCategory);
-
 module.exports = router;
